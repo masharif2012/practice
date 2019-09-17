104dbddd60184f77d9b78843ee2f4a8f40fb3a1e
masharif2012 <isharifpalash@gmail.com>
2019-09-17 15:09:39 -0400

    solving mathe prob.

---
+++ GeneralMathematics.java
@@ -0,0 +1,17 @@
+package mathSolvingPKG;
+
+public class GeneralMathematics {
+
+	public static void main(String[] args) {
+		// let's do some addition
+		
+		int x= 3;
+		int y= 5;
+		System.out.println(x+y);//adding 
+		System.out.println(x*y);// multiplication
+		System.out.println(y/x);//quotient using division operator
+		System.out.println(y%x);//remainder using modulus operator
+
+	}
+
+}
